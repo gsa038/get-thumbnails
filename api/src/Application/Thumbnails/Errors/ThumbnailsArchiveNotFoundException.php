c93ebc661afd5c0ed2ae2f79c988c2d4b7c23f7c
Stanislav Gnoyanko <gsa038@mail.ru>
2020-11-27 11:31:40 +0300

    upd

--- api/src/Application/Thumbnails/Errors/ThumbnailsArchiveNotFoundException.php
+++ api/src/Application/Thumbnails/Errors/ThumbnailsArchiveNotFoundException.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace App\Application\Thumbnails\Errors;
 
-use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpNotFoundException;
 
-class ThumbnailsArchiveNotFoundException extends HttpBadRequestException
+class ThumbnailsArchiveNotFoundException extends HttpNotFoundException
 {
     public function __construct() {
         $this->message = 'Requested archive not found.';
